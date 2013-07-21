d0307541fa51d0274f9e697ee94bc307cace4a68
Tim Baxter <tim.baxter@cerner.com>
2013-07-21 18:13:46 -0500

    URL paths

--- sample_project/django_test/urls.py
+++ sample_project/django_test/urls.py
@@ -15,5 +15,8 @@ urlpatterns = patterns(
 
     url(r'^articles/', include('articles.urls.article_urls')),
     url(r'^contact/', include('contact_manager.urls')),
-
+    url(r'^photos/', include('galleries.urls')),
+    url(r'^events/', include('happenings.urls')),
+    url(r'^profiles/', include('happenings.urls')),
+    url(r'^video/', include('happenings.urls')),
 )
