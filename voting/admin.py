61e59938e24fd9dd70856ba81356fef36b925563
Tim Baxter <tim.baxter@cerner.com>
2013-08-07 13:14:22 -0500

    Removed voting from apps. Pull from github fork instead.

--- voting/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from voting.models import Vote
-
-
-class VoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content_type', 'vote')
-
-admin.site.register(Vote, VoteAdmin)
