124b1aec429d7cc15d80f356c29ceab4b4997916
Tim Baxter <tim.baxter@cerner.com>
2013-08-21 13:30:20 -0500

    Packaging cleanup

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open('docs/requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name='django-tango',
-    version='0.1',
+    version='0.1.5',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
     description='Faster, simpler Django content management.',
     long_description=open('README.md').read(),
     url='https://github.com/tBaxter/Tango',
-    license='BSD license, see LICENSE.txt',
-    author=u'Tim Baxter',
-    author_email='mail.baxter@gmail.com',
+    license='LICENSE.txt',
+
     packages=find_packages(),
     include_package_data=True,
     install_requires=required,
-    #zip_safe=False,
+    zip_safe=True,
     dependency_links = ['http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1']
 )
