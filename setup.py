88f17f3c8d37c71ed4d8fe5ce4ebdc6d8fb86015
Tim Baxter <tim.baxter@cerner.com>
2013-08-23 09:20:13 -0500

    Submodule is pointless. Just install what we need.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=required,
     zip_safe=False,
     dependency_links = [
-        #'http://github.com/tBaxter/tango-articles/tarball/master#egg=tango-articles-0.2',
+        'http://github.com/tBaxter/tango-articles/tarball/master#egg=tango-articles-0.2',
         'http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1'
     ]
 )
