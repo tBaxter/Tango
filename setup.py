f0a8c80de3f4c0535cc9be38ffa84e1a3ee73fe2
Tim Baxter <tim.baxter@cerner.com>
2013-08-26 13:28:57 -0500

    moved contact manager out into reusable app

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     dependency_links = [
         'http://github.com/tBaxter/tango-shared-core/tarball/master#egg=tango-shared-0.3',
         'http://github.com/tBaxter/tango-articles/tarball/master#egg=tango-articles-0.2',
+        'http://github.com/tBaxter/tango-contact-manager/tarball/master#egg=tango-contact-0.3'
         'http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1'
     ]
 )
