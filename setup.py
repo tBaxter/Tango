cdb081c3ee67c7c69bd94b62dd511e07366beaac
Tim Baxter <tim.baxter@cerner.com>
2013-09-05 20:09:18 -0500

    Pulled out Happenings into reusable standalone app.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('docs/requirements.txt') as f:
 
 setup(
     name='django-tango',
-    version='0.3.1',
+    version='0.3.2',
     author=u'Tim Baxter',
     author_email='mail.baxter@gmail.com',
     description='Faster, simpler Django content management.',
@@ -23,5 +23,6 @@ setup(
         'http://github.com/tBaxter/tango-articles/tarball/master#egg=tango-articles-0.3',
         'http://github.com/tBaxter/tango-contact-manager/tarball/master#egg=tango-contact-0.4',
         'http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1'
+        'http://github.com/tBaxter/django-happenings/tarball/master#egg=tango-happenings-0.2'
     ]
 )
