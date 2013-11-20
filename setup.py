062e1533190f440ef406b02c45de2f8392a05c78
Tim Baxter <tim.baxter@cerner.com>
2013-11-20 09:55:13 -0600

    Updated to use split out tango-photos and updated tango-capo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('docs/requirements.txt') as f:
 
 setup(
     name='django-tango',
-    version='0.3.3',
+    version='0.4',
     author=u'Tim Baxter',
     author_email='mail.baxter@gmail.com',
     description='Faster, simpler Django content management.',
