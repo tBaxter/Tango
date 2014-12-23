7792bea5775c14f2304cabf593d74ccd0382aa0f
Tim Baxter <tim.baxter@cerner.com>
2014-12-23 15:29:26 -0600

    test settings

---
+++ test_settings.py
@@ -0,0 +1,30 @@
+SECRET_KEY = "lorem ipsum"
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+SITE_ID = 1
+
+#stripped down middleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ACTIVITY_MONITOR_MODELS = (
+    {
+        'model': 'auth.user',     # Required: the model to watch.
+        'verb': " joined ",
+    },
+)
