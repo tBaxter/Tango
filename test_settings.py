4e67932f38404cfa410efdc03684976b054c1a9c
TBaxter <mail.baxter@gmail.com>
2018-08-14 11:58:32 -0500

    added template settings

--- test_settings.py
+++ test_settings.py
@@ -24,17 +24,24 @@ SITE_ID = 1
 AUTH_USER_MODEL = 'tango_user.Profile'
 ROOT_URLCONF = 'test_urls'
 
-#stripped down middleware
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
 ACTIVITY_MONITOR_MODELS = (
     {
         'model': 'auth.user',     # Required: the model to watch.
         'verb': " joined ",
     },
 )
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
